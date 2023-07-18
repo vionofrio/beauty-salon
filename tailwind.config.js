a54c255ecdba7ca9a2780045ec77b71ee129f1a7
vionofrio <ovictor.valle@outlook.com>
2023-07-17 23:31:44 -0300

    feat: initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1120px",
+    },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        poppins: ["var(--font-poppins)"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "current",
+        white: "rgb(255 255 255 / <alpha-value>)",
+        brand: {
+          lighter: "var(--color-brand-lighter)",
+          light: "var(--color-brand-light)",
+          DEFAULT: "rgb(var(--color-brand) / <alpha-value>)",
+          dark: "var(--color-brand-dark)",
+        },
+        gray: {
+          light: "var(--color-gray-light)",
+          DEFAULT: "rgb(var(--color-gray) / <alpha-value>)",
+          dark: "var(--color-gray-dark)",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "section-divide-gradient":
+          "linear-gradient(to right, rgb(var(--color-brand)), var(--color-brand-lighter))",
       },
     },
   },
   plugins: [],
-}
+};
