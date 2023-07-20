440931ec7e0243b5c5cf002e05b5631de54c1aa4
vionofrio <ovictor.valle@outlook.com>
2023-07-20 15:13:47 -0300

    chore(app): change lang attribute to "pt-BR"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${sans.variable} ${poppins.variable} font-sans`}>
         {children}
       </body>
